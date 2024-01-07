cbfb90233aa201c675704724f3bf04cfb61b89ea
bachhungcb <bachhungcb@gmail.com>
2024-01-07 22:52:04 +0700

    add alert window

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 console.log('Hello');
 console.log('I like Pizza');
+
+// window.alert(`This is an alert`);
+// window.alert(`I like Pizza`);
+
+//this is a comment
+
+/*
+this 
+is
+a
+comment
+*/
