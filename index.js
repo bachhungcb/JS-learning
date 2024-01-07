1d920a7647b7ffc418ee08341753aee35cd1370f
bachhungcb <bachhungcb@gmail.com>
2024-01-07 23:10:00 +0700

    learn more about variables

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza`;
+let fullName = "Bro Code";
+let age = 25;
+let student = false;
 
-// let x = 123;
-// let age = 25;
-// let prize = 10.99;
-// let gpa = 2.1;
-
-// console.log(age);
-// console.log(prize);
-// console.log(gpa);
-// console.log(typeof gpa);
-// console.log(`You are ${age} years old`);
-// console.log(`The prize is ${prize}`);
-// console.log(`You GPA is ${gpa}`);
-
-// let firstName ="Bach";
-// let favoriteFood ="pizza";
-// let email = "Bro123@gmail.com"
-
-// console.log(typeof firstName);
-// console.log(firstName);
-// console.log(`you like ${favoriteFood}`);
-// console.log(`Your emails is ${email}`);
-
-let online = true;
-let forSale = true;
-let isStudent = true;
-
-console.log(typeof online);
-console.log(`Bro is online: ${online}`);
-console.log(`Is this car for sales: ${forSale}`);
-console.log(`Enrolled: ${isStudent}`);
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
