ed6f210b3fdc8d17a618d39d2bd22372946b4f71
bachhungcb <bachhungcb@gmail.com>
2024-01-07 22:53:32 +0700

    add hello to my page

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-console.log('Hello');
-console.log('I like Pizza');
-
-// window.alert(`This is an alert`);
-// window.alert(`I like Pizza`);
-
-//this is a comment
-
-/*
-this 
-is
-a
-comment
-*/
+document.getElementById("myH1").textContent = "Hello";
