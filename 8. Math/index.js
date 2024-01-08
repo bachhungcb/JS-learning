8b4895aa04e1730a86382b6c1ff2b8fb57dfc8cb
bachhungcb <bachhungcb@gmail.com>
2024-01-08 13:19:02 +0700

    Lear Math in JS

---
+++ 8. Math/index.js
@@ -0,0 +1,21 @@
+//Math
+
+let x = 3.21;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x);L\
+// z = Math.round(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x,y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+//let max = Math.max(x,y,z);
+let min = Math.min(x,y,z);
+console.log(min);
