9e603f58d28bd138687eece85b16374c5828aa14
bachhungcb <bachhungcb@gmail.com>
2024-01-08 12:26:37 +0700

    learn how to use const in javascript

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+const PI = 3.14159;
+let radius;
+let circumference;
 
-let y;
-let x;
-let z;
-
-x = Number(x);
-y = String(y);
-z= Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference =2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + " cm";    
+}
