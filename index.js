020ea0cdd37d8d98b73729ff835ddc621fcbdb63
bachhungcb <bachhungcb@gmail.com>
2024-02-08 00:05:52 +0700

    learn more about while loop

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // while loop = reapeat some code WHILE some condition is true
 
-let username = "";
+let login = true;
+let username;
+let password;
 
-while(username === "" || username === null){
-    
+while(!login){
+    username = window.prompt(`Enter your username`);
+    password = window.prompt(`Enter your password`);
+
+    if(username === "myUsername" && password === "myPassword"){
+        login = true;
+        console.log("You are logged in!");
+    }
+    else{
+        console.log("Wrong username or password. Please try again!");
+    }
 }
-
-console.log(`Hello ${username}`);
