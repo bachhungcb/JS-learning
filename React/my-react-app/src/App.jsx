ff74d4eaac5ffa641455677da63b5396ceaabb07
bachhungcb <bachhungcb@gmail.com>
2024-03-02 17:48:27 +0700

    learn how to use props in JS

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,9 +1,15 @@
-import Button from "./Button"
+import Student from './Student.jsx'
 
-function App() {
-  return(
-    <Button/>
-  );
+function App(){
+    return(
+        <>
+            <Student name="Bach Dam" age={20} isStudent={false}/>
+            <Student name="Ha Huy" age={-10} isStudent={true}/>
+            <Student name="Duc Hung" age={10} isStudent={false}/>
+            <Student name="Dam Huy" age={15} isStudent={true}/>
+            <Student name="Phu"/>
+        </>
+    );
 }
 
 export default App
