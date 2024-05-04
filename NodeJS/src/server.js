e57553038d704149082b07d8c5c5d363df413a11
bachhungcb <bachhungcb@gmail.com>
2024-05-04 11:27:28 +0700

    learn how to use env file

--- NodeJS/src/server.js
+++ NodeJS/src/server.js
@@ -1,27 +1,31 @@
-const express = require('express'); //commonjs
-const path = require('path');
+const express = require("express"); //commonjs
+const path = require("path");
+require("dotenv").config();
+
+console.log(">>> check env: ", process.env);
 
 const app = express(); // app express
-const port = 8080; //port
+const port = process.env.PORT || 8083; //port => hardcode
+const hostname = process.env.HOST_NAME || "localhost";
 
 //configure template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //khai bao route
-app.get('/', (req, res) => {
-  res.send('Hello World! \n hehehehe');
+app.get("/", (req, res) => {
+  res.send("Hello World! \n hehehehe");
 });
 
-app.get('/abc', (req, res) => {
-  res.send('Check ABC');
+app.get("/abc", (req, res) => {
+  res.send("Check ABC");
 });
 
-app.get('/bcd', (req, res) => {
+app.get("/bcd", (req, res) => {
   // res.send('<h1>avx</h1>');
-  res.render('sample.ejs');
+  res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
