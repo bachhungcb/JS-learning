1676a45faa811c7d0b6e3a249ce22060997d9762
bachhungcb <bachhungcb@gmail.com>
2024-05-04 20:00:42 +0700

    static files in express JS

--- NodeJS/src/server.js
+++ NodeJS/src/server.js
@@ -1,27 +1,30 @@
-const express = require("express"); //commonjs
-const path = require("path");
-require("dotenv").config();
+const express = require('express'); //commonjs
+const path = require('path');
+require('dotenv').config();
 
 const app = express(); // app express
 const port = process.env.PORT || 8083; //port => hardcode
-const hostname = process.env.HOST_NAME || "localhost";
+const hostname = process.env.HOST_NAME || 'localhost';
 
 //configure template engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//config static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //khai bao route
-app.get("/", (req, res) => {
-  res.send("Hello World! \n huhu");
+app.get('/', (req, res) => {
+  res.send('Hello World! \n huhu');
 });
 
-app.get("/abc", (req, res) => {
-  res.send("Check ABC");
+app.get('/abc', (req, res) => {
+  res.send('Check ABC');
 });
 
-app.get("/bcd", (req, res) => {
+app.get('/bcd', (req, res) => {
   // res.send('<h1>avx</h1>');
-  res.render("sample.ejs");
+  res.render('sample.ejs');
 });
 
 app.listen(port, hostname, () => {
