3be8720f38d969fe1573c0dc691258b9cb4941da
bachhungcb <bachhungcb@gmail.com>
2024-05-12 11:18:01 +0700

    add a nav bar

--- NodeJS/src/controllers/homeController.js
+++ NodeJS/src/controllers/homeController.js
@@ -3,15 +3,7 @@ const sqlConfig  = require('../config/database');
 
 
 const getHomepage = async (req, res) =>{
-    try {
-        // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect(sqlConfig)
-        const result = await sql.query`SELECT * FROM Users;`
-        user = result;
-       } catch (err) {
-        console.log(err)
-       }
-    res.send(JSON.stringify(user));
+    return res.render('home.ejs')
 }
 
 const getABC = (req, res) =>{
