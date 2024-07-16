e804475d771883e6f621118426730298d7d83be2
bachhungcb <bachhungcb@gmail.com>
2024-07-16 17:02:18 +0700

    write function for creating records

--- my-notes-app/notes-app-server/src/index.ts
+++ my-notes-app/notes-app-server/src/index.ts
@@ -21,6 +21,28 @@ app.get("/api/notes", async (req,res)=>{
         }
 })
 
+app.post("/api/notes", async (req,res)=>{
+    const { Title, Content } = req.body[0];
+    
+    if (!Title || !Content) {
+        return res.status(400).json({ error: "title and content fields required" });
+    }
+
+    try{
+        const note = await sql.connect(config);
+        await note.request()
+            .input('title', sql.NVarChar, Title)
+            .input('content', sql.NVarChar, Content)
+            .query('insert into notes (title, content) values (@title, @content)');
+            res.status(200).json({message: "Note added successfully"});
+    }catch(err){
+        console.log(err)
+    };
+})
+
+app.put("/api/notes/:id", async (req,res)=>{
+
+})
 
 app.listen(5000,() =>{
     console.log("Server is running on port 5000");
