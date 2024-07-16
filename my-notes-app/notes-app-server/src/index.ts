fdcf3aa9e3bea63f37f5566239b5c4ba5c284ddd
bachhungcb <bachhungcb@gmail.com>
2024-07-16 20:25:31 +0700

    function to delete notes

--- my-notes-app/notes-app-server/src/index.ts
+++ my-notes-app/notes-app-server/src/index.ts
@@ -41,8 +41,49 @@ app.post("/api/notes", async (req,res)=>{
 })
 
 app.put("/api/notes/:id", async (req,res)=>{
+    const { Title, Content } = req.body[0];
+    const id = parseInt(req.params.id);
+
+    if (!Title || !Content) {
+        return res.status(400).json({ error: "title and content fields required" });
+    }
+
+    if(!id || isNaN(id)){
+        return res.status(400).json({error: "id must be a number"});
+    }
+
+    try{
+        const note = await sql.connect(config);
+        await note.request()
+            .input('id', sql.Int, id)
+            .input('title', sql.NVarChar, Title)
+            .input('content', sql.NVarChar, Content)
+            .query('update notes set title = @title, content = @content where id = @id');
+            res.status(200).json({message: "Note updated successfully"});
+    }catch(err){
+        res.status(500).json({error: "Internal server error"});
+    };
 
 })
+
+app.delete("/api/notes/:id", async (req,res)=>{
+    const id = parseInt(req.params.id);
+
+    if(!id || isNaN(id)){
+        return res.status(400).json({error: "id must be a number"});
+    }
+
+    try{
+        const note = await sql.connect(config);
+        await note.request()
+                    .input('id', sql.Int, id)
+                    .query('delete from notes where id = @id');
+                    res.status(200).json({message: "Note deleted successfully"});   
+    }catch(err){
+        console.log(err);
+    }
+
+});
 
 app.listen(5000,() =>{
     console.log("Server is running on port 5000");
